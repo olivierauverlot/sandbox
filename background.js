359683c97b69fa9bda64ddff7d626c2685a24487
Olivier Auverlot <olivier.auverlot@gmail.com>
2014-07-13 21:03:18 +0200

    sandbox ok

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'bounds': {
-      'width': 800,
-      'height': 450
+      'width': 400,
+      'height': 400
     }
   });
 });
--- js/model.js
+++ js/model.js
@@ -1,4 +1,6 @@
 $(document).ready(
   function() {
+    var ret = eval("2 / 1");
+    document.getElementById('res').innerHTML = ret;
   }
 );

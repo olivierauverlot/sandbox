fa7d3d7a48c7bfff2ddd298ec11e638fd237b4a7
Olivier Auverlot <olivier.auverlot@gmail.com>
2014-07-16 10:49:45 +0200

    tests with filesystem

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
+
+  
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'bounds': {
+      'left': 500,
+      'top': 500,
       'width': 400,
       'height': 400
     }
--- js/model.js
+++ js/model.js
@@ -1,6 +1,31 @@
-$(document).ready(
-  function() {
-    var ret = eval("2 / 1");
-    document.getElementById('res').innerHTML = ret;
-  }
-);
+document.addEventListener('DOMContentLoaded', function() {
+  var ret = eval("2 / 1");
+  // document.getElementById('res').innerHTML = ret;
+  var chosenFileEntry = null;
+
+  chooseFileButton = document.getElementById('btnfileselector');
+  console.log("DEBUG");
+  
+  chooseFileButton.addEventListener('click', function(e) {
+    var accepts = [{
+      mimeTypes: ['text/*'],
+      extensions: ['js', 'css', 'txt', 'html', 'xml', 'tsv', 'csv', 'rtf']
+    }];
+    chrome.fileSystem.chooseEntry({type: 'openFile', accepts: accepts}, function(readOnlyEntry) {
+   
+      readOnlyEntry.file(function(file) {
+        var reader = new FileReader();
+  
+        reader.onerror = errorHandler;
+        reader.onloadend = function(e) {
+          console.log(e.target.result);
+        };
+    
+        reader.readAsText(file);
+      });
+  	});
+  });
+});
+
+
+
